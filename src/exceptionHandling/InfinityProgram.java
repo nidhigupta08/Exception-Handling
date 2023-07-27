a1f547ecf3b720a934be0bd07754a6a1eadbc25a
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-07-27 19:35:47 +0530

    Infinity

---
+++ src/exceptionHandling/InfinityProgram.java
@@ -0,0 +1,13 @@
+package exceptionHandling;
+
+public class InfinityProgram {
+
+	public static void main(String[] args) 
+	{	
+	 double n[] = {3.2, 3, 4, 6};
+	 int a = 4;
+	 double x = n[0]/(a - n[2]);
+	 
+	System.out.println(x);
+	 }
+}
