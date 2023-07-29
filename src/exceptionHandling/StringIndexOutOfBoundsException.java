a07b3cb6f97604d8794d77de4f8d3e74089e8388
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-07-29 13:12:30 +0530

    example

---
+++ src/exceptionHandling/StringIndexOutOfBoundsException.java
@@ -0,0 +1,12 @@
+package exceptionHandling;
+
+public class StringIndexOutOfBoundsException {
+
+	public static void main(String[] args) {
+		 String str = "Scientech";
+		 System.out.println(str.charAt(9));
+
+
+	}
+
+}
